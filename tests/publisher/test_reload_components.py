05bb9a692ff2973ee7c0ba1c0e8dc255e154298c
marco-buttu <marco.buttu@gmail.com>
2020-03-26 10:59:17 +0000

    Fix issue 86

--- tests/publisher/test_reload_components.py
+++ tests/publisher/test_reload_components.py
@@ -5,10 +5,11 @@ import suricate.component
 from mock import patch, mock_open
 
 
-user_config = """
+def get_user_config(startup_delay=0):
+    return """
 COMPONENTS:
   TestNamespace/Positioner02:
-    startup_delay: 0
+    startup_delay: %d
     container: PositionerContainer
     methods:
     - name: getPosition
@@ -19,7 +20,7 @@ COMPONENTS:
     - name: current
       timer: 0.1
   TestNamespace/Positioner03:
-    startup_delay: 0
+    startup_delay: %d
     container: PositionerContainer
     properties:
     - name: current
@@ -30,13 +31,63 @@ HTTP:
 SCHEDULER:
   reschedule_error_interval: 0.4
   reschedule_interval: 0.2
-"""
+""" % (startup_delay, startup_delay)
+
+
+
+
+def test_startup_delay(Publisher, redis_client, logger):
+    """The component will be active after startup_delay"""
+    try:  # Load the user configuration
+        startup_delay =  3 # seconds
+        user_config = get_user_config(startup_delay)
+        from suricate import configuration
+        func = "suricate.configuration.open"
+        with patch(func, mock_open(read_data=user_config)) as f:
+            reload(configuration)
+            f.assert_called_with(configuration.config_file)
+            from suricate.configuration import config
+
+        p = Publisher(config['COMPONENTS'])
+        p.start()
+        time.sleep(2)
+        # Components not available before startup_delay
+        message = redis_client.hget('TestNamespace/Positioner02/position', 'error')
+        assert message.endswith('startup in progress')
+        message = redis_client.hget('TestNamespace/Positioner02/current', 'error')
+        assert message.endswith('startup in progress')
+        message = redis_client.hget('TestNamespace/Positioner02/getPosition', 'error')
+        assert message.endswith('startup in progress')
+        message = redis_client.hget('TestNamespace/Positioner03/current', 'error')
+        assert message.endswith('startup in progress')
+
+        time.sleep(startup_delay)  # Wait for the startup to terminate
+
+        message = redis_client.hget('TestNamespace/Positioner00/position', 'error')
+        assert not message
+        message = redis_client.hget('TestNamespace/Positioner00/current', 'error')
+        assert not message
+        message = redis_client.hget('TestNamespace/Positioner00/getPosition', 'error')
+        assert not message
+        message = redis_client.hget('TestNamespace/Positioner01/current', 'error')
+        assert not message
+
+        lines = open(logger.file_name).readlines()
+        assert len(lines) == 4
+        assert 'startup in progress' in lines[0]  # First component startup
+        assert 'startup in progress' in lines[1]  # Second component startup
+        assert 'is online' in lines[2]  # First component online
+        assert 'is online' in lines[3]  # Second component online
+
+    finally:
+        reload(configuration)
 
 
 def test_all_components_not_active_at_startup(Publisher, redis_client):
     """In case all components are not available at suricate startup,
     they have to be loaded as soon as they become available."""
     try:  # Load the user configuration
+        user_config = get_user_config()
         from suricate import configuration
         func = "suricate.configuration.open"
         with patch(func, mock_open(read_data=user_config)) as f:
@@ -79,6 +130,7 @@ def test_some_components_not_active_at_startup(Publisher, redis_client):
     """In case some components are not available at suricate startup,
     they have to be loaded as soon as they become available."""
     try:  # Load the user configuration
+        user_config = get_user_config()
         from suricate import configuration
         func = "suricate.configuration.open"
         with patch(func, mock_open(read_data=user_config)) as f:
